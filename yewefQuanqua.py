d65be59678db90d816d90df8776b5d01039b5c55
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-03-18 19:10:40 +0300

    init yewefquanqua

---
+++ yewefQuanqua.py
@@ -0,0 +1,9 @@
+def letterToNumber(m):
+    return string.ascii_lowercase.index(m)
+
+def encrypt(plainText,key):
+    # Find amharic character and if charachter is multiple of 1 find the multiple of 1 and add it
+    
+    return plainText
+def decrypt(cipherText,key):
+    return cipherText
