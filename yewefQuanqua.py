b9912eab9241df3da2016c44d08ae94144253dd5
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-03-18 20:04:27 +0300

    finished the encryption and decryption of yewefquanqua

--- yewefQuanqua.py
+++ yewefQuanqua.py
@@ -1,9 +1,24 @@
+import string
+ha = "ሇ"
 def letterToNumber(m):
     return string.ascii_lowercase.index(m)
-
+print(ord(ha))
 def encrypt(plainText,key):
-    # Find amharic character and if charachter is multiple of 1 find the multiple of 1 and add it
-    
-    return plainText
-def decrypt(cipherText,key):
+    cipherText = ""
+    for letter in plainText:
+        vowel = (ord(letter)-4608)%8
+        cipherText+=(letter+chr(ord(key)+vowel))
     return cipherText
+def decrypt(cipherText,key):
+    plainText = ""
+    even = True
+    for letter in cipherText:
+        if(even):
+            plainText+=letter
+            even = False
+        else: 
+            even = True
+    return plainText
+
+print(encrypt("ለሉሊላሌልሎሏ","ሀ"))
+print(decrypt("ለሀሉሁሊሂላሃሌሄልህሎሆሏሇ","U"))
